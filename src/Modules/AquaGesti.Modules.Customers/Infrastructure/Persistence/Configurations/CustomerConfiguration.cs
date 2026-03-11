096eda424fccd0e28566b60066f86b04c1ba554b
Javier Aguilar <jaguilar@syp.es>
2026-03-11 19:59:22 +0100

    [Add] Customers infrastructure with DbContext and entity configuration

---
+++ src/Modules/AquaGesti.Modules.Customers/Infrastructure/Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AquaGesti.Modules.Customers.Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AquaGesti.Modules.Customers.Infrastructure.Persistence.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+        }
+    }
+}
---
+++ src/Modules/AquaGesti.Modules.Customers/Infrastructure/Persistence/CustomersDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AquaGesti.Modules.Customers.Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+
+namespace AquaGesti.Modules.Customers.Infrastructure.Persistence
+{
+    public class CustomersDbContext : DbContext
+    {
+        public DbSet<Customer> Customers => Set<Customer>();
+
+        public CustomersDbContext(DbContextOptions<CustomersDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CustomersDbContext).Assembly);
+        }
+    }
+}
